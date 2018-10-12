98d43ff05e92a16c1b6b34790f1d07e0cc426fde
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-12 22:14:38 +0300

    Fixed code.

--- look_and_say.rb
+++ look_and_say.rb
@@ -1,14 +1,12 @@
 # @param [String] str
 # @return [String]
 def look_and_say(str)
-  if str.length < 2
-    return "1" + str
-  end
+  return '1' + str if str.length < 2
 
-  result = ""
+  result = ''
   times = 1
   repeatable = str[0]
-  str = str.slice(1..-1) + " "
+  str = str.slice(1..-1) + ' '
   str.each_char do |c|
     if c == repeatable
       times += 1
@@ -21,8 +19,8 @@ def look_and_say(str)
   result
 end
 
-str = "1"
-for i in 1..15
+str = '1'
+10.times do
   puts str
   str = look_and_say(str)
 end
